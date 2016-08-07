c2c787feb79d2b7978945f4640bcc7d964536ac7
SleekPanther <macrobyte2@gmail.com>
2016-08-07 07:51:21 -0400

    top start

---
+++ non-pages/php-include/top.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <!--[if lt IE 9]>
+            <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+        <![endif]-->
+        <meta name="robots" content="index, follow">
+        <meta name="author" content="Your Name"><!-- Add your name/company -->
+        <meta name="description" content="Your description">
+        
+        <?php
+//=============IMPORTANT variable $ROOT_DIRECTORY must be where the project is housed (where the homepage is). ==========================
+        $ROOT_DIRECTORY = "assignment7";        //MUST CHANGE THIS OR THE ENTIRE PROJECT WON'T WORK!
+        
+        //======Magical code to display errors========\\
+        //======COMMENT OUT WHEN YOU'RE FINISHED TESTING========\\
+        //error_reporting(E_ALL);           //longer version = 2 lines
+        //ini_set('display_errors', '1');
+        ini_set('error_reporting', E_ALL);  //short version
+        
+        ?>
