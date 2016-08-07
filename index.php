cbdcd918923967db3d96ec65f34c554c92977ecf
SleekPanther <macrobyte2@gmail.com>
2016-08-07 07:31:37 -0400

    basic homepage template

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include("non-pages/php-include/top.php"); ?><!-- MUST MANUALLY LINK TO top.php & USING ../ but use $upFolderPlaceHolder from now on -->
+    <h1 class="cent"><?php echo $pageTitle . $tagLine; ?></h1><!-- homepage can be special and also print the tagline if needed -->
+    <p>text</p>
+</main><!-- close main, opened in header.php -->
+
+<?php include($upFolderPlaceholder . "non-pages/php-include/footer.php"); ?>
+</body>
+</html>
