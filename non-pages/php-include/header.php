f602f8c1ded6cdb939b7709b73aca67e4e1d0e5b
SleekPanther <macrobyte2@gmail.com>
2016-08-07 07:39:36 -0400

    basic header

---
+++ non-pages/php-include/header.php
@@ -0,0 +1,7 @@
+<!-- header.php -->
+<header>
+    <p>Header (logo, title, slideshow, etc. or optional)</p>
+</header>
+
+<main id="actualMainContent"><!-- this div begins the actual content but is actually in header.php -->
+<!-- end header.php -->
