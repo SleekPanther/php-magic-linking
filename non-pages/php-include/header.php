8e5bae14d0ff95252152b3cc0906b0f57cb248f7
SleekPanther <macrobyte2@gmail.com>
2016-08-07 14:23:59 -0400

    breadcrumbs function, no link text yet

--- non-pages/php-include/header.php
+++ non-pages/php-include/header.php
@@ -4,6 +4,20 @@
 </header>
 
 <!--breadcrumbs trail -->
+<section class="breadcrumbs">
+    <?php
+        if($folderCountAdjusted != 0){      //don't bother with breadcrumbs on the homepage
+            $tempBreadLink = $domain . '/';
+            for($i = 0; $i < $folderCountAdjusted; $i++){
+                $tempBreadLink .= $split_url_adjusted[$i] .'/';
+                echo '<a href="' . $tempBreadLink . '" >' .$tempBreadLink  .'</a> > ';
+            }
+
+            echo '<strong>' . $pageTitle . '</strong>';
+        }
+        
+    ?>
+</section>
 
 <main id="actualMainContent"><!-- id for accessibility skip button. this div begins the actual content but is actually in header.php -->
 <!-- end header.php -->
