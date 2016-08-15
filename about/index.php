5f6360f53b50c081aada2fcb4ad89a101dd3f8fd
SleekPanther <macrobyte2@gmail.com>
2016-08-15 13:56:20 -0400

    more indents

--- about/index.php
+++ about/index.php
@@ -1,7 +1,7 @@
 <?php include("../non-pages/php-include/top.php"); ?><!-- MUST MANUALLY LINK TO top.php & USING ../ but use $upFolderPlaceHolder from now on -->
-    <h1 class="cent"><?php echo $pageTitle ?></h1><!-- display page title -->
+	    <h1 class="cent"><?php echo $pageTitle ?></h1><!-- display page title -->
 
-    <p>Integer sodales tellus congue tortor imperdiet, ullamcorper vulputate lacus laoreet. Curabitur mollis rhoncus ex, quis volutpat orci sodales eu. Nunc augue ipsum, ullamcorper et fringilla sit amet, lacinia sit amet ante. Nam nulla leo, commodo vel semper in, egestas eu risus. Donec sed congue urna. Aliquam commodo porta mattis. Quisque vulputate sed nulla nec accumsan. Donec viverra libero ex, ultricies pellentesque libero tristique eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam et laoreet nulla. In scelerisque risus ligula, at sagittis massa vehicula in. Nam pretium augue in dui varius, ac pharetra enim feugiat. Nunc scelerisque sem sed gravida aliquam. Suspendisse sed dolor ligula. Aliquam molestie commodo tellus nec varius.</p>
-</main><!-- close main, opened in header.php -->
+	    <p>Integer sodales tellus congue tortor imperdiet, ullamcorper vulputate lacus laoreet. Curabitur mollis rhoncus ex, quis volutpat orci sodales eu. Nunc augue ipsum, ullamcorper et fringilla sit amet, lacinia sit amet ante. Nam nulla leo, commodo vel semper in, egestas eu risus. Donec sed congue urna. Aliquam commodo porta mattis. Quisque vulputate sed nulla nec accumsan. Donec viverra libero ex, ultricies pellentesque libero tristique eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam et laoreet nulla. In scelerisque risus ligula, at sagittis massa vehicula in. Nam pretium augue in dui varius, ac pharetra enim feugiat. Nunc scelerisque sem sed gravida aliquam. Suspendisse sed dolor ligula. Aliquam molestie commodo tellus nec varius.</p>
+	</main><!-- close main, opened in header.php -->
 
 <?php include($upFolderPlaceholder . "non-pages/php-include/footer.php"); ?>
--- portfolio/examples/index.php
+++ portfolio/examples/index.php
@@ -1,9 +1,9 @@
 <?php include("../../non-pages/php-include/top.php"); ?><!-- MUST MANUALLY LINK TO top.php & USING ../ but use $upFolderPlaceHolder from now on -->
-    <h1 class="cent"><?php echo $pageTitle ?></h1><!-- display page title -->
-	
-	<h4>Kind of hidden page. Accessible via breadcrumbs but not the menu dropdown.</h4>
-	
-    <p>Integer sodales tellus congue tortor imperdiet, ullamcorper vulputate lacus laoreet. Curabitur mollis rhoncus ex, quis volutpat orci sodales eu. Nunc augue ipsum, ullamcorper et fringilla sit amet, lacinia sit amet ante. Nam nulla leo, commodo vel semper in, egestas eu risus. Donec sed congue urna. Aliquam commodo porta mattis. Quisque vulputate sed nulla nec accumsan. Donec viverra libero ex, ultricies pellentesque libero tristique eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam et laoreet nulla. In scelerisque risus ligula, at sagittis massa vehicula in. Nam pretium augue in dui varius, ac pharetra enim feugiat. Nunc scelerisque sem sed gravida aliquam. Suspendisse sed dolor ligula. Aliquam molestie commodo tellus nec varius.</p>
-</main><!-- close main, opened in header.php -->
+	    <h1 class="cent"><?php echo $pageTitle ?></h1><!-- display page title -->
+		
+		<h4>Kind of hidden page. Accessible via breadcrumbs but not the menu dropdown.</h4>
+		
+	    <p>Integer sodales tellus congue tortor imperdiet, ullamcorper vulputate lacus laoreet. Curabitur mollis rhoncus ex, quis volutpat orci sodales eu. Nunc augue ipsum, ullamcorper et fringilla sit amet, lacinia sit amet ante. Nam nulla leo, commodo vel semper in, egestas eu risus. Donec sed congue urna. Aliquam commodo porta mattis. Quisque vulputate sed nulla nec accumsan. Donec viverra libero ex, ultricies pellentesque libero tristique eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam et laoreet nulla. In scelerisque risus ligula, at sagittis massa vehicula in. Nam pretium augue in dui varius, ac pharetra enim feugiat. Nunc scelerisque sem sed gravida aliquam. Suspendisse sed dolor ligula. Aliquam molestie commodo tellus nec varius.</p>
+	</main><!-- close main, opened in header.php -->
 
 <?php include($upFolderPlaceholder . "non-pages/php-include/footer.php"); ?>
